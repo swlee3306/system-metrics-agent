636176138148d77fe5293bbc2002a2604fb7d3ce
su.lee <su.lee@okestro.com>
2025-03-21 09:59:09 +0900

    네트워크 정보 API 추가 및 관련 구조체 정의

---
+++ pkg/config/config_network.go
@@ -0,0 +1,11 @@
+package config
+
+// 네트워크 인터페이스 정보를 저장할 구조체
+type NetworkInterfaceInfo struct {
+	Interfaces []struct {
+		Name         string `json:"name"`          // 인터페이스 이름
+		MacAddress   string `json:"mac_address"`   // MAC 주소
+		BondingGroup string `json:"bonding_group"` // 본딩 그룹명 (없으면 빈 문자열)
+		BondingMac   string `json:"bonding_mac"`   // 본딩 MAC 주소 (없으면 빈 문자열)
+	} `json:"NetworkInterfaces"`
+}
